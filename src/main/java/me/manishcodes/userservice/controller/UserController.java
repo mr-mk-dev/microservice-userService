de37ef567a592bc906fea0abe36a231ee775671d
Manish <itsmenishan.12@gmail.com>
2025-11-18 00:22:01 +0530

    added new packages

---
+++ src/main/java/me/manishcodes/userservice/controller/UserController.java
@@ -0,0 +1,7 @@
+package me.manishcodes.userservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ src/main/java/me/manishcodes/userservice/service/UserService.java
@@ -0,0 +1,7 @@
+package me.manishcodes.userservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
